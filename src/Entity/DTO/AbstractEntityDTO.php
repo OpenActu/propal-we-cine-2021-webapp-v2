7cd261379498ed42b1417b2390cb2bdae5683053
yanroussel <yanroussel@gmail.com>
2024-11-06 17:45:39 +0100

    Standardisation design pattern singleton

--- src/Entity/DTO/AbstractEntityDTO.php
+++ src/Entity/DTO/AbstractEntityDTO.php
@@ -4,7 +4,8 @@ namespace App\Entity\DTO;
 
 use App\Utils\DesignPattern\Singleton\Singleton;
 use App\Contracts\{EntityInterface, EntityDTOInterface, SerializerInterface};
+use App\Contracts\DesignPattern\SingletonInterface;
 
-abstract class AbstractEntityDTO extends Singleton implements EntityInterface, EntityDTOInterface, SerializerInterface {
+abstract class AbstractEntityDTO extends Singleton implements EntityInterface, EntityDTOInterface, SerializerInterface, SingletonInterface {
 
 }
--- src/Utils/DesignPattern/Singleton/Singleton.php
+++ src/Utils/DesignPattern/Singleton/Singleton.php
@@ -1,11 +1,10 @@
 <?php 
 namespace App\Utils\DesignPattern\Singleton;
 
-use App\Contracts\EntityInterface;
 use App\Contracts\DesignPattern\SingletonInterface;
 
-abstract class Singleton implements SingletonInterface {
-    public static function getInstance(...$args): EntityInterface { 
+abstract class Singleton  {
+    public static function getInstance(...$args): SingletonInterface { 
         $classname = get_called_class();
         return new $classname(...$args);
     }
